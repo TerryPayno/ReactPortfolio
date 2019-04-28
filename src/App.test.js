6e476dbc1e641ccd200ac6635c9a3b65ffc4e994
James Payne <jwepayne@hotmail.co.uk>
2019-04-28 23:05:32 +0100

    Added axios and http request

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Home/Home';
+import Work from './Work/Work';
+import Contact from './Contact/Contact';
+import Nav from './Nav/Nav';
+import About from './About/About';
+import { BrowserRouter as Router } from 'react-rotuer-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+  <Router>
+  <Nav />
+  </Router>, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<About />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Contact />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Work />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import './Contact.css';
+import axios from 'axios';
 
-function Contact() {
-  return (
-    <body>
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: '',
+                  content: ''};
+
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleEmailSend = this.handleEmailSend.bind(this);
+  }
+  handleContentChange(e) {
+    this.setState({content: e.target.value})
+  }
+  handleEmailChange(e) {
+    this.setState({email: e.target.value})
+  }
+  handleEmailSend() {
+    console.log("Content " + this.state.content);
+    console.log("Email " + this.state.email);
+    axios.post('http://localhost:55336/api/Charge/Test123?', this.state)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error) 
+    })
+  };
+
+  render () {
+    return (<body>
         <div class="title">
            <h1>Contact Page</h1>
         </div>
         <div class="form">
             <p class="email">Email:</p>
-            <input/>
+            <input type="text" name="emailAddress" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange}/>
             <p>Email Content</p>
-            <textarea class="emailCont"></textarea>
+            <textarea class="emailCont" type="text" name="emailAddress" placeholder="Email" value={this.state.content} onChange={this.handleContentChange}></textarea>
             <p>Phone Number:</p>
             <p>07582 831 923</p>
             <p>Address:<br/><br/>
@@ -20,11 +49,11 @@ function Contact() {
                 West Midlands,<br/>
                 CV3 1PH
             </p>
-            
+            <button type="buttonm" onClick={this.handleEmailSend}>Send</button>
         </div>
-    </body>
-
-  );
-}
+    </body>);
+  
+};
+};
 
 export default Contact;
